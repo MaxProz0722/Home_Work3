73ec11aaaee157e444a7949bb535bef63229d3de
MaxProz0722 <maxproz22@gmail.com>
2024-11-06 19:14:20 +0300

    module_1_3.py

---
+++ module_3_3.py
@@ -0,0 +1,17 @@
+def print_params(*params, a = 1, b = 'строка', c = True):
+    print(*params, a, b, c)
+
+
+print_params()
+print_params( 1, 2, 3, 4, 5, 6, a = 2, b = 3, c = 5)
+print_params(b = 25)
+print_params(c = [1,2,3])
+
+value_list = ['@', 2, float]
+value_dict = {'a': True, 'b': 2, 'c': 'yo'}
+
+print_params(*value_list)
+print_params(**value_dict)
+
+values_list_2 = ['yes', False]
+print_params(*values_list_2, 42)
