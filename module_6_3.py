0af67773941a8fc9502594a4c3bee34c2b5a65c8
max <maproz22@gmail.com>
2025-01-07 18:04:55 +0300

    shlypa

--- module_6_3.py
+++ module_6_3.py
@@ -1,48 +1,66 @@
 import random
 
+
 class Animal:
     live = True
     sound = None
     _DEGREE_OF_DANGER = 0
-    def __init__(self, _cords, speed):
+
+    def __init__(self, speed):
         self._cords = [0, 0, 0]
         self.speed = speed
 
     def move(self, dx, dy, dz):
-        self._cords = [dx, dy, dz]
-        for self._cords in self._cords:
-            self.speed * self._cords
-
-        if dz < 0:
-            self._cords[2] / self.speed
+        if self._cords[2] + dz * self.speed < 0:
             print("It's too deep, i can't dive :(")
+        else:
+            self._cords[0] += dx * self.speed
+            self._cords[1] += dy * self.speed
+            self._cords[2] += dz * self.speed
 
     def get_cords(self):
-        print(f"X: {self._cords[0]}, Y: {self._cords[1]}, Z: {self._cords[2]}")
+        print(f"X: {self._cords[0]} Y: {self._cords[1]} Z: {self._cords[2]}")
 
     def attack(self):
         if self._DEGREE_OF_DANGER < 5:
-            print("Sorry, i'm paceful :)")
-        elif self._DEGREE_OF_DANGER >= 5:
-            print("Be careful, i'm attacking you 0_0" )
+            print("Sorry, i'm peaceful :)")
+        else:
+            print("Be careful, i'm attacking you 0_0")
+
 
 class Bird(Animal):
     beak = True
 
+
+    # @staticmethod
     def lay_eggs(self):
-        print(f"Here are(is) {random.choice(1, 4)} eggs for you")
+        eggs = random.randint(1, 4)
+        print(f"Here are(is) {eggs} eggs for you")
 
 class AquaticAnimal(Animal):
     _DEGREE_OF_DANGER = 3
-    def dive_in(self, dz):
-        dz = dz
 
+
+    def dive_in(self, dz):
+        dzn = abs(dz)
+        self._cords[2] = dzn / 2
 
 class PoisonousAnimal(Animal):
     _DEGREE_OF_DANGER = 8
 
-class Duckbill(Bird, AquaticAnimal, PoisonousAnimal):
+    def __init__(self, speed):
+        super().__init__(speed)
+
+class Duckbill(PoisonousAnimal, AquaticAnimal, Bird):
     sound = "Click-click-click"
+
+    def __init__(self, speed):
+        super().__init__(speed)
+
+
+    def speak(self):
+        print(self.sound)
+
 
 
 db = Duckbill(10)
